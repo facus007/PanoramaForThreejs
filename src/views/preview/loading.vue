3b4e62bb8681c4658d9425b04d5cf3ef9e376d7a
kirigiri hitomi <chenjw@hiscene.com>
2020-06-11 16:49:01 +0800

    loading 修改

--- src/views/preview/loading.vue
+++ src/views/preview/loading.vue
@@ -1,39 +1,25 @@
 <template>
-  <div class="loading" :style="style"/>
+  <div class="loading"/>
 </template>
 <script>
 var count = 1
 export default {
-  data(){return {
-    style: {},
-    src: null,
-    curframe: 0,
-    disposed: false,
-  }},
-  methods:{
-    keyframe(){
-      if(++count % 2 === 0){
-        let str ='00000'+this.curframe;
-        this.curframe = (this.curframe + 1) % 43
-        let index = str.substring(str.length-5,str.length);
-        let name = './static/loading-anime/png序列帧OK_'+index+'.png'
-        this.src = name;
-      }
-      !this.disposed && requestAnimationFrame(this.keyframe)
-      return name
-    }
-  },
-  mounted(){
-    requestAnimationFrame(this.keyframe)
-  },
-  beforeDestroy(){
-    this.disposed = true
-  }
+  data(){return {}},
+  methods:{},
+  mounted(){},
+  beforeDestroy(){}
 }
 </script>
 <style scoped>
 .loading{
   width: 200px;
   height: 200px;
+  background-image: url(../../assets/img/loading_sprite.png);
+  -webkit-animation:animation 1s steps(41) 0s infinite both;
+  animation: animation 1s steps(41) 0s infinite both;
+}
+
+@-webkit-keyframes animation {
+  to { background-position: -4100% 0}
 }
 </style>
