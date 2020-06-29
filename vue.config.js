5e1b1e7023e2410c1c75ab98f12ea0da632585b3
kirigiri hitomi <chenjw@hiscene.com>
2020-06-29 18:21:44 +0800

    close optimization

--- vue.config.js
+++ vue.config.js
@@ -10,28 +10,26 @@ module.exports = {
   publicPath: './',
   pluginOptions: {},
   configureWebpack: config => {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    config.optimization = {
-      runtimeChunk: 'single',
-      splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: Infinity,
-        minSize: 20000,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name (module) {
-              // get the name. E.g. node_modules/packageName/not/this/part.js
-              // or node_modules/packageName
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-              // npm package names are URL-safe, but some servers don't like @ symbols
-              return `package.${packageName.replace('@', '')}`
-            }
-          }
-        }
-      }
-    }
+    // config.optimization = {
+    //   runtimeChunk: 'single',
+    //   splitChunks: {
+    //     chunks: 'all',
+    //     maxInitialRequests: Infinity,
+    //     minSize: 20000,
+    //     cacheGroups: {
+    //       vendor: {
+    //         test: /[\\/]node_modules[\\/]/,
+    //         name (module) {
+    //           // get the name. E.g. node_modules/packageName/not/this/part.js
+    //           // or node_modules/packageName
+    //           const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+    //           // npm package names are URL-safe, but some servers don't like @ symbols
+    //           return `package.${packageName.replace('@', '')}`
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
     config.plugins.push(new CompressionPlugin({test: /\.js$|\.html$|\.css/, threshold: 10240, deleteOriginalAssets: false}))
   },
   pages: {
