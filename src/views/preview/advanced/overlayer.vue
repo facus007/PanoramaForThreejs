2af57fe2f4cc7787616b099f4ef41606b140b583
kirigiri hitomi <chenjw@hiscene.com>
2020-07-20 19:56:10 +0800

    fix overlayer flash

--- src/views/preview/advanced/overlayer.vue
+++ src/views/preview/advanced/overlayer.vue
@@ -1,6 +1,7 @@
 <template>
   <transition name="el-fade-in">
-    <div v-if="visible" style="width: 100%; height: 100%; top: 0; left: 0; position: absolute; z-index: 2000; align-items: center; visibility: visible;">
+    <el-dialog :visible.sync="visible" append-to-body>
+    <div v-if="visible" style="width: 100%; height: 100%; top: 0; left: 0; position: fixed; align-items: center; visibility: visible;">
       <div style="width:100%;height:100%; background: #000B; display:grid; justify-items:center; align-items:center; grid-auto-flow:row; grid-template-rows: 1fr 50px;" >
         <div style="width:100%;height:100%;">
           <div style="width:100%;height:100%; position:absolute; padding:20px 20px 50px 20px; display:flex; justify-content:center; align-items:center;">
@@ -10,6 +11,7 @@
         <el-button icon="el-icon-close" type="text" style="width:30px; height:30px; z-index:5; padding: 0;margin:10px; border-radius: 30px; background:#444;" @click="visible=false"/>
       </div>
     </div>
+  </el-dialog>
   </transition>
 </template>
 
@@ -23,12 +25,6 @@ export default {
     visible(next, pre){this.$emit('input', next);},
     value(next, pre){this.visible = next;},
   },
-  mounted(){
-    document.body.appendChild(this.$el)
-  },
-  beforeDestroy(){
-    document.body.removeChild(this.$el)
-  }
 }
 </script>
 
