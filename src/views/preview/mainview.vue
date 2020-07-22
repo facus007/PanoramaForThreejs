a2278a66dc1882dd3c1f557a36621df8fce92aaa
kirigiri hitomi <chenjw@hiscene.com>
2020-07-22 15:44:33 +0800

    css3 open advance

--- src/views/preview/mainview.vue
+++ src/views/preview/mainview.vue
@@ -12,7 +12,7 @@
       <camera-animation v-if="!after_animation_loaded && !loading" :fov="curScene.fov" :start_rotation="curScene.start_rotation" :product="product"/>
 
       <preview v-if="curScene && !loading" :curScene="curScene" :key="curSceneId" :visible="after_animation_loaded" style="visibility: hidden"/>
-      <!-- <advanced v-if="!loading" style="position: absolute; top: 0; right: 0; z-index:2;" :product="product"/> -->
+      <advanced v-if="!loading" style="position: absolute; top: 0; right: 0; z-index:2;" :product="product"/>
     </THREE>
     <div v-if="loading" style="position: absolute; width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; z-index: 5;">
       <i v-if="first_loaded" style="font-size: 20px; color: white; text-shadow: 0 0 5px;" class="el-icon-loading"/>
