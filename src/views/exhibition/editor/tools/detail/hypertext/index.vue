26cd345a7df560784db59c8ed2f96a9ebf2784c9
kirigiri hitomi <chenjw@hiscene.com>
2020-07-29 17:29:41 +0800

    fix hypertext in admesh

--- src/views/exhibition/editor/tools/detail/hypertext/index.vue
+++ src/views/exhibition/editor/tools/detail/hypertext/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div style="display: flex; flex-direction: column; width: 100%; height:120px; align-items: flex-start;">
-    <span v-if="curedit.embeddings[0].hotspots.indexOf(selected) < 0" style="display: grid;width:100%; grid-template-areas: 'e e' 'c b' 'd a'; grid-column-gap: 4px; grid-row-gap: 5px;align-items: center; justify-items: flex-end;">
+  <div style="display: flex; flex-direction: column; width: 100%; height:120px; align-items: flex-start; gap: 5px 4px;">
+    <el-button type="primary" size="mini" style="margin:0; width: 100%;" @click="showEditor=true">编 辑</el-button>
+    <span v-if="curedit.embeddings[0].hotspots.indexOf(selected) < 0" style="display: grid;width:100%; grid-template-areas:'c b' 'd a'; grid-column-gap: 4px; grid-row-gap: 5px;align-items: center; justify-items: flex-end;">
       <el-button type="primary" size="mini" style="width:100%;margin:0; grid-area: c;" @click="showSpotDialog=true">修改呼吸灯</el-button>
       <el-button type="primary" size="mini" style="width:100%;margin:0; grid-area: b;" @click="clearSpot">默认呼吸灯</el-button>
       <el-checkbox v-model="hidespot" style="margin:0;color:gray; grid-area: a;">隐藏呼吸灯</el-checkbox>
-      <el-button type="primary" size="mini" style="margin:0; width: 100%; grid-area: e;" @click="showEditor=true">编 辑</el-button>
     </span>
     <editor v-model="showEditor" @content="setContent" :item="selected.target"/>
     <material-selector v-model="showSpotDialog" @select="selectSpot" imgtype="1"/>
